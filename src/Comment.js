edda58f126fe33c08d0e24ea9ae0df00ab3f4de4
Krzych <krzych.pierzchala@gmail.com>
2018-01-07 18:59:05 +0100

    Added thumb-down comments vote

--- src/Comment.js
+++ src/Comment.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Comment = ({text, votes, id, getThumbUp}) => {
+const Comment = ({text, votes, id, getThumbUp, getThumbDown}) => {
 	return (
 		<li>
 			{text} <span>votes: {votes}</span>
 			<button onClick={() => getThumbUp(id)}>Thumb up</button>
+			<button onClick={() => getThumbDown(id)}>Thumb down</button>
 		</li>
 	);
 };
--- src/CommentContainer.js
+++ src/CommentContainer.js
@@ -1,9 +1,10 @@
-import { connect } from 'redux';
+import { connect } from 'react-redux';
 import Comment from './Comment';
-import { getThumbUp } from './actions';
+import { getThumbUp, getThumbDown } from './actions';
 
 const mapDispatchToProps = dispatch => ({
-  getThumbUp: (id) => dispatch(getThumbUp(id))
+  getThumbUp: (id) => dispatch(getThumbUp(id)),
+  getThumbDown: (id) => dispatch(getThumbDown(id))
 });
 
 export default connect(null, mapDispatchToProps)(Comment);
