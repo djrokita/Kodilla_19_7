4f026d886ce9d92324b6fa87e612900de3bbdf0a
Krzych <krzych.pierzchala@gmail.com>
2018-01-09 20:44:50 +0100

    Removed dispatch for editComment action

--- src/CommentContainer.js
+++ src/CommentContainer.js
@@ -6,8 +6,7 @@ import './Comment.css';
 const mapDispatchToProps = dispatch => ({
   voteUp: (id) => dispatch(getThumbUp(id)),
   voteDown: (id) => dispatch(getThumbDown(id)),
-  deleteComment: (id) => dispatch(removeComment(id)),
-  edit: (text, id) => dispatch(editComment(text, id))
+  deleteComment: (id) => dispatch(removeComment(id))
 });
 
 export default connect(null, mapDispatchToProps)(Comment);
