4309dd8fa31239885dfbebace581eb88a419398d
Krzych <krzych.pierzchala@gmail.com>
2018-01-10 07:06:17 +0100

    Removed unneeded js code

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './reducer';
-import  { addComment } from './actions';
 
 const store = createStore(reducer);
 
